5a288f782cdca2dde69e6f3866f393a4d20cd403
SVolowy4 <121169529+SVolowy4@users.noreply.github.com>
2024-07-29 09:42:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const $body = document.getElementById('body')
 
 const $mainDisplay = document.getElementById('mainDisplay')
 
+window.onload = function() {
+  document.getElementById('preloader').style.display = 'none';
+}
+
+
 //створення головних кнопок
 function creatBtn(bc, b) {
   
